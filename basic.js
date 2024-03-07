ad3a484815f3004072a75df1fb657cba9e3563eb
iamvishaldev <vishalyadavdev1@gmail.com>
2024-03-07 22:28:06 +0530

    Working on 2d array

---
+++ basic.js
@@ -0,0 +1,29 @@
+// 1. Write a JS code to print numbers from 1 to 10
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log("i", i)
+// }
+
+// 2.Write a JS code to reverse a number from 10 to 1
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log("i", i)
+// }
+
+//3.Write a JS code to print a 2D array
+
+// Example 2D array
+
+const oneDArray = [1, 2, 3, 4]
+
+const twoDArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+for (let i = 0; i < oneDArray.length; i++) {
+    console.log("i", i)
+}
+
+console.log("result", oneDArray.length)
